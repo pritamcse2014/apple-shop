4fe46f63b7cef82dbf3fc35d648e9c364eed1f40
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-01-10 17:41:35 +0600

    first and completed commit

---
+++ app/Http/Middleware/TokenAuthenticate.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Helper\JWTToken;
+use App\Helper\ResponseHelper;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TokenAuthenticate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $token = $request->cookie('token');
+        $result = JWTToken::ReadToken($token);
+        if ($result == "unauthorized")
+        {
+//            return redirect("/userLogin");
+            return ResponseHelper::Out('Unauthorized', null, 401);
+        }
+        else
+        {
+            $request->headers->set('email', $result->userEmail);
+            $request->headers->set('id', $result->userID);
+            return $next($request);
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,88 @@
+<?php
+
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\PolicyController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\TokenAuthenticate;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+//Home Page
+Route::get('/', [HomeController::class, 'HomePage']);
+Route::get('/by-category', [CategoryController::class, 'ByCategoryPage']);
+Route::get('/by-brand', [BrandController::class, 'ByBrandPage']);
+Route::get('/policy', [PolicyController::class, 'PolicyPage']);
+Route::get('/details', [ProductController::class, 'Details']);
+Route::get('/login', [UserController::class, 'LoginPage']);
+Route::get('/verify', [UserController::class, 'VerifyPage']);
+Route::get('/wish', [ProductController::class, 'WishList']);
+Route::get('/cart', [ProductController::class, 'CartListPage']);
+Route::get('/profile', [ProfileController::class, 'ProfilePage']);
+
+//Brand List
+Route::get('/brandList', [BrandController::class, 'BrandList']);
+
+//Category List
+Route::get('/categoryList', [CategoryController::class, 'CategoryList']);
+
+//Product List
+Route::get('/listProductByCategory/{id}', [ProductController::class, 'ListProductByCategory']);
+Route::get('/listProductByBrand/{id}', [ProductController::class, 'ListProductByBrand']);
+Route::get('/listProductByRemark/{remark}', [ProductController::class, 'ListProductByRemark']);
+
+//Slider
+Route::get('/listProductSlider', [ProductController::class, 'ListProductSlider']);
+
+//Product Details
+Route::get('/productDetailsById/{id}', [ProductController::class, 'ProductDetailsById']);
+Route::get('/listReviewByProduct/{product_id}', [ProductController::class, 'ListReviewByProduct']);
+
+//Policy
+Route::get('/policyType/{type}', [PolicyController::class, 'PolicyByType']);
+
+//User Auth
+Route::get('/userLogin/{UserEmail}', [UserController::class, 'UserLogin']);
+Route::get('/verifyLogin/{UserEmail}/{OTP}', [UserController::class, 'VerifyLogin']);
+Route::get('/logout', [UserController::class, 'UserLogout']);
+
+//User Profile
+Route::post('/createProfile', [ProfileController::class, 'CreateProfile'])->middleware([TokenAuthenticate::class]);
+Route::get('/readProfile', [ProfileController::class, 'ReadProfile'])->middleware([TokenAuthenticate::class]);
+
+//Product Review
+Route::post('/createProductReview', [ProductController::class, 'CreateProductReview'])->middleware([TokenAuthenticate::class]);
+
+//Product Wish
+Route::get('/productWishList', [ProductController::class, 'ProductWishList'])->middleware([TokenAuthenticate::class]);
+Route::get('/createWishList/{product_id}', [ProductController::class, 'CreateWishList'])->middleware([TokenAuthenticate::class]);
+Route::get('/removeWishList/{product_id}', [ProductController::class, 'RemoveWishList'])->middleware([TokenAuthenticate::class]);
+
+//Product Cart
+Route::post('/createCartList', [ProductController::class, 'CreateCartList'])->middleware([TokenAuthenticate::class]);
+Route::get('/cartList', [ProductController::class, 'CartList'])->middleware([TokenAuthenticate::class]);
+Route::get('/deleteCartList/{product_id}', [ProductController::class, 'DeleteCartList'])->middleware([TokenAuthenticate::class]);
+
+//Invoice and Payment
+Route::get('/invoiceCreate', [InvoiceController::class, 'InvoiceCreate'])->middleware([TokenAuthenticate::class]);
+Route::get('/invoiceList', [InvoiceController::class, 'InvoiceList'])->middleware([TokenAuthenticate::class]);
+Route::get('/invoiceProductList/{invoice_id}', [InvoiceController::class, 'InvoiceProductList'])->middleware([TokenAuthenticate::class]);
+
+//Payment
+Route::post('/paymentSuccess', [InvoiceController::class, 'PaymentSuccess']);
+Route::post('/paymentCancel', [InvoiceController::class, 'PaymentCancel']);
+Route::post('/paymentFail', [InvoiceController::class, 'PaymentFail']);
