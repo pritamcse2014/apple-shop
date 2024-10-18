8542d4c4d3ff700790afa11c8902936f575c11d1
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-10-18 08:28:00 +0600

    Back-End-Create-Migration-Files-For-Database

--- database/migrations/2024_01_02_042337_create_brands.php
+++ database/migrations/2024_01_02_042337_create_brands.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('brands', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('brandName', 50);
+            $table->string('brandImg', 300);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
         });
     }
 
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('brands');
     }
 };
