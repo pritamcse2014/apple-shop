b5db3d296d4a2fd3c18b3fe78270d666ab65996c
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-10-18 09:47:09 +0600

    Back-End-Brand-Category-Product-Policy-Invoice-Profile-User-Controller-and-Backend-Testing

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helper\ResponseHelper;
+use App\Models\Product;
+use App\Models\ProductDetails;
+use App\Models\ProductReview;
+use App\Models\ProductSlider;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function ListProductByCategory(Request $request):JsonResponse
+    {
+        $data = Product::where('category_id', $request->id)->with('brand', 'category')->get();
+        return ResponseHelper::Out('success', $data, 200);
+    }
+
+    public function ListProductByRemark(Request $request):JsonResponse
+    {
+        $data = Product::where('remark', $request->remark)->with('brand', 'category')->get();
+        return ResponseHelper::Out('success', $data, 200);
+    }
+
+    public function ListProductByBrand(Request $request):JsonResponse
+    {
+        $data = Product::where('brand_id', $request->id)->with('brand', 'category')->get();
+        return ResponseHelper::Out('success', $data, 200);
+    }
+
+    public function ListProductSlider(Request $request):JsonResponse
+    {
+        $data = ProductSlider::all();
+        return ResponseHelper::Out('success', $data, 200);
+    }
+
+    function ProductDetailsById(Request $request):JsonResponse
+    {
+        $data = ProductDetails::where('product_id', $request->id)->with('product', 'product.brand', 'product.category')->get();
+        return ResponseHelper::Out('success', $data, 200);
+    }
+
+    function ListReviewByProduct(Request $request):JsonResponse
+    {
+        $data = ProductReview::where('product_id', $request->product_id)->with(['profile' => function ($query) {
+            $query->select('id', 'cus_name');
+        }])->get();
+        return ResponseHelper::Out('success', $data, 200);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\BrandController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PolicyController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +20,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//  Brand List
+Route::get('/brandList', [BrandController::class, 'BrandList']);
+
+//  Category List
+Route::get('/categoryList', [CategoryController::class, 'CategoryList']);
+
+//  Product List
+Route::get('/listProductByCategory/{id}', [ProductController::class, 'ListProductByCategory']);
+Route::get('/listProductByBrand/{id}', [ProductController::class, 'ListProductByBrand']);
+Route::get('/listProductByRemark/{remark}', [ProductController::class, 'ListProductByRemark']);
+
+//  Slider
+Route::get('/listProductSlider', [ProductController::class, 'ListProductSlider']);
+
+//  Product Details
+Route::get('/productDetailsById/{id}', [ProductController::class, 'ProductDetailsById']);
+Route::get('/listReviewByProduct/{product_id}', [ProductController::class, 'ListReviewByProduct']);
+
+//  Policy
+Route::get('/policyType/{type}', [PolicyController::class, 'PolicyByType']);
