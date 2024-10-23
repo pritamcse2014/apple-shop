b872018f7e6e620fe013ea586411d80ba3512bdb
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-10-23 20:52:46 +0600

    Home-Slider

--- resources/views/component/HeroSlider.blade.php
+++ resources/views/component/HeroSlider.blade.php
@@ -2,24 +2,40 @@
 <div class="banner_section slide_medium shop_banner_slider staggered-animation-wrap">
     <div id="carouselExampleControls" class="carousel slide carousel-fade light_arrow" data-bs-ride="carousel">
         <div id="carouselSection" class="carousel-inner">
-            <div class="carousel-item active background_bg active" style="background-image: url('https://img.freepik.com/free-photo/bright-toy-paper-shirts-near-shopping-trolley-sale-tags_23-2147957037.jpg?t=st=1729606641~exp=1729610241~hmac=e5106813bac8947d6c4606118fa86a0fefe379b117392b7f26ab55a40c08f2c6&w=996')">
-                <div class="banner_slide_content">
-                    <div class="container"><!-- STRART CONTAINER -->
-                        <div class="row">
-                            <div class="col-lg-7 col-9">
-                                <div class="banner_content overflow-hidden">
-                                    <h5 class="mb-3 staggered-animation font-weight-light" data-animation="slideInLeft" data-animation-delay="0.5s">Get up to 50% off Today Only!</h5>
-                                    <h2 class="staggered-animation" data-animation="slideInLeft" data-animation-delay="1s">Woman Fashion</h2>
-                                    <a class="btn btn-fill-out rounded-0 staggered-animation text-uppercase" href="shop-left-sidebar.html" data-animation="slideInLeft" data-animation-delay="1.5s">Shop Now</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div><!-- END CONTAINER-->
-                </div>
-            </div>
+
         </div>
         <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-bs-slide="prev"><i class="ion-chevron-left"></i></a>
         <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-bs-slide="next"><i class="ion-chevron-right"></i></a>
     </div>
 </div>
 <!-- END SECTION BANNER -->
+
+<script>
+    Hero()
+    async function Hero() {
+        let res = await axios.get("/listProductSlider");
+        $("#carouselSection").empty();
+        res.data['data'].forEach((item,i)=>{
+            let activeClass=''
+            if(i===0){
+                activeClass=' active '
+            }
+            let SliderItem=`<div class="carousel-item  background_bg ${activeClass}" style="background-image: url('${item.image}')">
+                <div class="banner_slide_content">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-7 col-9">
+                                <div class="banner_content overflow-hidden">
+                                    <h5 class="mb-3 staggered-animation font-weight-light" data-animation="slideInLeft" data-animation-delay="0.5s">${item.price}</h5>
+                                    <h2 class="staggered-animation" data-animation="slideInLeft" data-animation-delay="1s">${item.title}</h2>
+                                    <a class="btn btn-fill-out rounded-0 staggered-animation text-uppercase" href="#" data-animation="slideInLeft" data-animation-delay="1.5s">Shop Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+            $("#carouselSection").append(SliderItem)
+        })
+    }
+</script>
