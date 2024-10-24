fe28839f79ce8835a58b9c56096d90f74f9a77f0
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2024-10-24 09:02:46 +0600

    Popular-Top-New-Special-Trending-Products

--- resources/views/component/ExclusiveProducts.blade.php
+++ resources/views/component/ExclusiveProducts.blade.php
@@ -29,12 +29,47 @@
             </div>
             <div class="tab-content">
                 <div class="tab-pane fade show active" id="Popular" role="tabpanel" aria-labelledby="arrival-tab">
-                    <div id="PopularItem" class="row shop_container">
-                        <div class="col-lg-3 col-md-4 col-6">
+                    <div id="popularItem" class="row shop_container">
+
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="New" role="tabpanel" aria-labelledby="sellers-tab">
+                    <div id="newItem"  class="row shop_container">
+
+
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="Top" role="tabpanel" aria-labelledby="featured-tab">
+                    <div id="topItem" class="row shop_container">
+
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="Special" role="tabpanel" aria-labelledby="special-tab">
+                    <div id="specialItem" class="row shop_container">
+
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="Trending" role="tabpanel" aria-labelledby="special-tab">
+                    <div id="trendingItem"  class="row shop_container">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    Popular();
+    async function Popular() {
+        let res = await axios.get('/listProductByRemark/popular');
+        $('#popularItem').empty();
+        res.data['data'].forEach((item, i) => {
+            let eachItem = `<div class="col-lg-3 col-md-4 col-6">
                             <div class="product">
                                 <div class="product_img">
                                     <a href="#">
-                                        <img src="https://img.freepik.com/free-vector/golden-frame-with-glitter-scatter_1017-30125.jpg?t=st=1729606751~exp=1729610351~hmac=79c203e36704cf15996566bc6710fc05b6995bc503bcd58b4b511b897a83daa3&w=996" alt="product_img9">
+                                        <img src="${item.image}" alt="product_img9">
                                     </a>
                                     <div class="product_action_box">
                                         <ul class="list_none pr_action_btn">
@@ -45,42 +80,163 @@
                                     </div>
                                 </div>
                                 <div class="product_info">
-                                    <h6 class="product_title"><a href="#">Title</a></h6>
+                                    <h6 class="product_title"><a href="#">${item.title}</a></h6>
                                     <div class="product_price">
-                                        <span class="price">$300</span>
+                                        <span class="price">$ ${item.price}</span>
                                     </div>
                                     <div class="rating_wrap">
                                         <div class="rating">
-                                            <div class="product_rate" style="width: 80%"></div>
+                                            <div class="product_rate" style="width: ${item.star}%"></div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="tab-pane fade" id="New" role="tabpanel" aria-labelledby="sellers-tab">
-                    <div id="NewItem"  class="row shop_container">
+                        </div>`
+            $('#popularItem').append(eachItem);
+        });
+    }
 
+    New();
+    async function New() {
+        let res = await axios.get('/listProductByRemark/new');
+        $('#newItem').empty();
+        res.data['data'].forEach((item, i) => {
+            let eachItem = `<div class="col-lg-3 col-md-4 col-6">
+                            <div class="product">
+                                <div class="product_img">
+                                    <a href="#">
+                                        <img src="${item.image}" alt="product_img9">
+                                    </a>
+                                    <div class="product_action_box">
+                                        <ul class="list_none pr_action_btn">
+                                            <li class="add-to-cart"><a href="#"><i class="icon-basket-loaded"></i>Add To Cart</a></li>
+                                            <li><a href="#" class="popup-ajax"><i class="icon-magnifier-add"></i></a></li>
+                                            <li><a href="#"><i class="icon-heart"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="product_info">
+                                    <h6 class="product_title"><a href="#">${item.title}</a></h6>
+                                    <div class="product_price">
+                                        <span class="price">$ ${item.price}</span>
+                                    </div>
+                                    <div class="rating_wrap">
+                                        <div class="rating">
+                                            <div class="product_rate" style="width: ${item.star}%"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>`
+            $('#newItem').append(eachItem);
+        });
+    }
 
-                    </div>
-                </div>
-                <div class="tab-pane fade" id="Top" role="tabpanel" aria-labelledby="featured-tab">
-                    <div id="TopItem" class="row shop_container">
+    Top();
+    async function Top() {
+        let res = await axios.get('/listProductByRemark/top');
+        $('#topItem').empty();
+        res.data['data'].forEach((item, i) => {
+            let eachItem = `<div class="col-lg-3 col-md-4 col-6">
+                            <div class="product">
+                                <div class="product_img">
+                                    <a href="#">
+                                        <img src="${item.image}" alt="product_img9">
+                                    </a>
+                                    <div class="product_action_box">
+                                        <ul class="list_none pr_action_btn">
+                                            <li class="add-to-cart"><a href="#"><i class="icon-basket-loaded"></i>Add To Cart</a></li>
+                                            <li><a href="#" class="popup-ajax"><i class="icon-magnifier-add"></i></a></li>
+                                            <li><a href="#"><i class="icon-heart"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="product_info">
+                                    <h6 class="product_title"><a href="#">${item.title}</a></h6>
+                                    <div class="product_price">
+                                        <span class="price">$ ${item.price}</span>
+                                    </div>
+                                    <div class="rating_wrap">
+                                        <div class="rating">
+                                            <div class="product_rate" style="width: ${item.star}%"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>`
+            $('#topItem').append(eachItem);
+        });
+    }
 
-                    </div>
-                </div>
-                <div class="tab-pane fade" id="Special" role="tabpanel" aria-labelledby="special-tab">
-                    <div id="SpecialItem" class="row shop_container">
+    Special();
+    async function Special() {
+        let res = await axios.get('/listProductByRemark/special');
+        $('#specialItem').empty();
+        res.data['data'].forEach((item, i) => {
+            let eachItem = `<div class="col-lg-3 col-md-4 col-6">
+                            <div class="product">
+                                <div class="product_img">
+                                    <a href="#">
+                                        <img src="${item.image}" alt="product_img9">
+                                    </a>
+                                    <div class="product_action_box">
+                                        <ul class="list_none pr_action_btn">
+                                            <li class="add-to-cart"><a href="#"><i class="icon-basket-loaded"></i>Add To Cart</a></li>
+                                            <li><a href="#" class="popup-ajax"><i class="icon-magnifier-add"></i></a></li>
+                                            <li><a href="#"><i class="icon-heart"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="product_info">
+                                    <h6 class="product_title"><a href="#">${item.title}</a></h6>
+                                    <div class="product_price">
+                                        <span class="price">$ ${item.price}</span>
+                                    </div>
+                                    <div class="rating_wrap">
+                                        <div class="rating">
+                                            <div class="product_rate" style="width: ${item.star}%"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>`
+            $('#specialItem').append(eachItem);
+        });
+    }
 
-                    </div>
-                </div>
-                <div class="tab-pane fade" id="Trending" role="tabpanel" aria-labelledby="special-tab">
-                    <div id="TrendingItem"  class="row shop_container">
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+    Trending();
+    async function Trending() {
+        let res = await axios.get('/listProductByRemark/trending');
+        $('#trendingItem').empty();
+        res.data['data'].forEach((item, i) => {
+            let eachItem = `<div class="col-lg-3 col-md-4 col-6">
+                            <div class="product">
+                                <div class="product_img">
+                                    <a href="#">
+                                        <img src="${item.image}" alt="product_img9">
+                                    </a>
+                                    <div class="product_action_box">
+                                        <ul class="list_none pr_action_btn">
+                                            <li class="add-to-cart"><a href="#"><i class="icon-basket-loaded"></i>Add To Cart</a></li>
+                                            <li><a href="#" class="popup-ajax"><i class="icon-magnifier-add"></i></a></li>
+                                            <li><a href="#"><i class="icon-heart"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="product_info">
+                                    <h6 class="product_title"><a href="#">${item.title}</a></h6>
+                                    <div class="product_price">
+                                        <span class="price">$ ${item.price}</span>
+                                    </div>
+                                    <div class="rating_wrap">
+                                        <div class="rating">
+                                            <div class="product_rate" style="width: ${item.star}%"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>`
+            $('#trendingItem').append(eachItem);
+        });
+    }
+</script>
